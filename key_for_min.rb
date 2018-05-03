2fda123a8f4044dbccd2f1060f91e3e4f2436e6d
Leslie Stevens <flylady@mail.utexas.edu>
2018-05-03 21:28:29 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,10 +3,9 @@
 
 def key_for_min_value(name_hash)
   values = name_hash.collect do|key value|
-    key_array.push(key)
-    value_array.push(value)
-    
+    key
+    value
   end
-  lowest = value.min
+
 
 end
